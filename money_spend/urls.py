0159511e7f625d094cfa8b6cfeddd9ab5a06fcda
Francisco Palacios <fjpc23022000@gmail.com>
2024-07-03 12:39:57 -0500

    Swagger docs now main page.

--- money_spend/urls.py
+++ money_spend/urls.py
@@ -7,12 +7,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Money Spend API",
         default_version='v1',
-        description="Test description",
+        description="This API allows you to efficiently track your expenses by including key components such as the title or name of the expense, different categories to classify the expense (e.g., food, transportation, entertainment), the total amount spent, support for multiple currencies, and any additional details or notes regarding the expense. This structure helps in managing and analyzing your expenses effectively, providing a clear overview of your spending habits.",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
+        contact=openapi.Contact(email="fjpc23022000@gmail.com"),
+        license=openapi.License(name="MIT License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,)
@@ -21,8 +21,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0),
          name='schema-json'),
-    path('docs/', schema_view.with_ui('swagger',
-                                      cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
                                        cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
